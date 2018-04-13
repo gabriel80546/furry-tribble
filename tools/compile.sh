a6ab561bf26940b31dec4850d7d8a51e7f73994e
gabriel80546 <gabriel80546@gmail.com>
2018-04-13 17:41:56 -0300

    this is a good comment

--- tools/compile.sh
+++ tools/compile.sh
@@ -13,56 +13,56 @@ echo -e '\033[7mcat ../Fen.c | sed "s|/\*YCM\*/|//|" > temp/Fen.c\033[0m'
 cat ../Posicao.c | sed "s|/\*YCM\*/|//|" > temp/Posicao.c
 echo -e "\033[7mgcc -Og -g -o ../engine ../engine.c\033[0m"
 echo -e "\$? = $?"
-gcc -Werror -Og -g -o  ../engine ./temp/engine.c
-if [ $? -eq 0 ]
+gcc -Og -g -o  ../engine ./temp/engine.c 2>err
+if [ $? -eq 0 -a ! -s err ]
 then
 	echo -e "\033[1;32mCompilaçao deu certo\033[0m"
 else
 	echo -e "\033[1;31mCompilação deu merda\033[0m"
 fi
 echo -e "\033[7mgcc -O1 -g -o engineO1 ../engine.c\033[0m"
-gcc -Werror -O1 -g -o  z/engineO1 ./temp/engine.c
-if [ $? -eq 0 ]
+gcc -O1 -g -o  z/engineO1 ./temp/engine.c 2>err
+if [ $? -eq 0 -a ! -s err ]
 then
 	echo -e "\033[1;32mCompilaçao deu certo\033[0m"
 else
 	echo -e "\033[1;31mCompilação deu merda\033[0m"
 fi
 echo -e "\033[7mgcc -O2 -g -o engineO2 ../engine.c\033[0m"
-gcc -Werror -O2 -g -o  z/engineO2 ./temp/engine.c
-if [ $? -eq 0 ]
+gcc -O2 -g -o  z/engineO2 ./temp/engine.c 2>err
+if [ $? -eq 0 -a ! -s err ]
 then
 	echo -e "\033[1;32mCompilaçao deu certo\033[0m"
 else
 	echo -e "\033[1;31mCompilação deu merda\033[0m"
 fi
 echo -e "\033[7mgcc -O3 -g -o engineO3 ../engine.c\033[0m"
-gcc -Werror -O3 -g -o  z/engineO3 ./temp/engine.c
-if [ $? -eq 0 ]
+gcc -O3 -g -o  z/engineO3 ./temp/engine.c 2>err
+if [ $? -eq 0 -a ! -s err ]
 then
 	echo -e "\033[1;32mCompilaçao deu certo\033[0m"
 else
 	echo -e "\033[1;31mCompilação deu merda\033[0m"
 fi
 echo -e "\033[7mgcc -O0 -g -o engineO0 ../engine.c\033[0m"
-gcc -Werror -O0 -g -o  z/engineO0 ./temp/engine.c
-if [ $? -eq 0 ]
+gcc -O0 -g -o  z/engineO0 ./temp/engine.c 2>err
+if [ $? -eq 0 -a ! -s err ]
 then
 	echo -e "\033[1;32mCompilaçao deu certo\033[0m"
 else
 	echo -e "\033[1;31mCompilação deu merda\033[0m"
 fi
 echo -e "\033[7mgcc -Os -g -o engineOs ../engine.c\033[0m"
-gcc -Werror -Os -g -o  z/engineOs ./temp/engine.c
-if [ $? -eq 0 ]
+gcc -Os -g -o  z/engineOs ./temp/engine.c 2>err
+if [ $? -eq 0 -a ! -s err ]
 then
 	echo -e "\033[1;32mCompilaçao deu certo\033[0m"
 else
 	echo -e "\033[1;31mCompilação deu merda\033[0m"
 fi
 echo -e "\033[7mgcc -Ofast -g -o engineOfast ../engine.c\033[0m"
-gcc -Werror -Ofast -g -o  z/engineOfast ./temp/engine.c
-if [ $? -eq 0 ]
+gcc -Ofast -g -o  z/engineOfast ./temp/engine.c 2>err
+if [ $? -eq 0 -a ! -s err ]
 then
 	echo -e "\033[1;32mCompilaçao deu certo\033[0m"
 else
